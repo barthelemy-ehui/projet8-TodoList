cf45c29efcf2b62f8def2189961044985a1fe98b
Barthelemy <barthelemy.ehui@gmail.com>
2018-12-11 09:11:32 +0100

    add pphunit coverage

---
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bart
+ * Date: 10/12/2018
+ * Time: 22:29
+ */
+
+namespace Tests\AppBundle\Controller;
+
+
+class TaskControllerTest
+{
+    
+}
---
+++ tests/AppBundle/Form/TaskTypeTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bart
+ * Date: 10/12/2018
+ * Time: 22:32
+ */
+
+namespace Tests\AppBundle\Form;
+
+
+class TaskTypeTest
+{
+    
+}
