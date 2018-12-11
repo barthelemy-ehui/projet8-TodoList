f3e5cc8d1047ad99d4198c4b77f93849aaa79246
Barthelemy <barthelemy.ehui@gmail.com>
2018-12-11 09:14:40 +0100

    add unity test on TaskType and UserType

--- tests/AppBundle/Form/TaskTypeTest.php
+++ tests/AppBundle/Form/TaskTypeTest.php
@@ -1,15 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 10/12/2018
- * Time: 22:32
- */
-
 namespace Tests\AppBundle\Form;
 
+use AppBundle\Form\TaskType;
+use Symfony\Component\Form\Test\TypeTestCase;
 
-class TaskTypeTest
+class TaskTypeTest extends TypeTestCase
 {
-    
+    public function testSubmitValidData()
+    {
+        $formData = [
+            'title' => 'une tâche',
+            'content' => 'le contenu de ma tâche',
+        ];
+        
+        $form = $this->factory->create(TaskType::class);
+        
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        
+        $view = $form->createView();
+        $children = $view->children;
+        
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
 }
--- tests/AppBundle/Form/UserTypeTest.php
+++ tests/AppBundle/Form/UserTypeTest.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 10/12/2018
- * Time: 22:32
- */
-
 namespace Tests\AppBundle\Form;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Symfony\Component\Form\Test\TypeTestCase;
 
-class UserTypeTest
+class UserTypeTest extends TypeTestCase
 {
-    
+    public function testSubmitValidData()
+    {
+        $formData = [
+            'username' => 'johndoe',
+            'password' => '123',
+            'email' => 'johndoe@gmail.com',
+            'role' => User::ROLE_USER,
+        ];
+        
+        $form = $this->factory->create(UserType::class);
+        
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        
+        $view = $form->createView();
+        $children = $view->children;
+        
+        foreach(array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+        
+    }
 }
