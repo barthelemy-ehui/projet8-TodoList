96f50e633deaccfdca46f164b8829be71d1e649d
Barthelemy <barthelemy.ehui@gmail.com>
2018-12-11 09:13:51 +0100

    add test unity on entity Task and user

--- tests/AppBundle/Entity/TaskTest.php
+++ tests/AppBundle/Entity/TaskTest.php
@@ -7,13 +7,46 @@ use PHPUnit\Framework\TestCase;
 
 class TaskTest extends TestCase
 {
+    public $task;
+    
+    public function setUp()
+    {
+        $this->task = new Task();
+        $this->task->setTitle('This is a new task');
+        $this->task->setContent('Put some content in it');
+        $this->task->setCreatedAt(new \DateTime('now'));
+    }
+    
+    public function testIsTaskHasIdNull(){
+        $this->assertNull($this->task->getId());
+    }
+    
+    public function testIsTaskHasTitle()
+    {
+        $this->assertNotEmpty($this->task->getTitle());
+    }
+    
+    public function testIsTaskHasContent()
+    {
+        $this->assertNotEmpty($this->task->getContent());
+    }
+    
+    public function testIsTaskHasCreatedAt()
+    {
+        $this->assertNotEmpty($this->task->getCreatedAt());
+    }
+    
+    public function testIsTaskHasDone()
+    {
+        $this->task->toggle(true);
+        $this->assertTrue($this->task->isDone());
+    }
+    
     public function testIsTaskBelongToUser()
     {
-           $task = new Task();
            $user = new User();
-           $task->setUser($user);
-           
-           $this->assertInstanceOf(User::class, $task->getUser());
+           $this->task->setUser($user);
+           $this->assertInstanceOf(User::class, $this->task->getUser());
     }
     
 }
