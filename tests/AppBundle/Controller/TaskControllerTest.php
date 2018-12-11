beacec694167ab3f90bcea9607acbb0659539128
Barthelemy <barthelemy.ehui@gmail.com>
2018-12-11 09:13:15 +0100

    add empty test controller

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 10/12/2018
- * Time: 22:29
- */
-
 namespace Tests\AppBundle\Controller;
+
 
 
 class TaskControllerTest
--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 10/12/2018
- * Time: 22:29
- */
-
 namespace Tests\AppBundle\Controller;
-
 
 class UserControllerTest
 {
